70c4cc3a9528c90a3869f0cb35fcbbdbe423e3a2
botko3 <112144946+botko3@users.noreply.github.com>
2024-02-03 00:34:52 +0800

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-const _filenameNew = fileURLToPath(import.meta.url);
-const _dirnameNew=path.dirname(_filenameNew);
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack'); // 用于访问内置插件
 
-export default{
-    entry:'./index.js',
-    target:"node",
-    output:{
-        path:path.resolve(_dirnameNew,'dist'),
-        filename:'bundle.js'
-    }
-}
+const _filenameNew = fileURLToPath(import.meta.url);
+const _dirname=path.dirname(_filenameNew);
+
+const path = require('path');
+
+module.exports = {
+  mode:'production',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'my-first-webpack.bundle.js',
+  },
+  module: {
+    rules: [{ test: /\.txt$/, use: 'raw-loader' }],
+  },
+};
